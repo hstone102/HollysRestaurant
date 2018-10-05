19c9fa5c7682d46e520c03ff368e3ca4558ce959
Holly Stone <hstone102@gmail.com>
2018-10-05 15:36:46 -0400

    it all works

--- dist/js/sun.js
+++ dist/js/sun.js
@@ -8,6 +8,32 @@ let sunMenuItem7 = document.getElementById('sunMenuItem7');
 let sunMenuItem8 = document.getElementById('sunMenuItem8');
 let sunMenuItem9 = document.getElementById('sunMenuItem9');
 let sunMenuItem10 = document.getElementById('sunMenuItem10');
+let sunMenuItem11 = document.getElementById('sunMenuItem11');
+let sunMenuItem12 = document.getElementById('sunMenuItem12');
+let sunSide1 = document.getElementById('side1');
+let sunSide2 = document.getElementById('side2');
+let sunSide3 = document.getElementById('side3');
+let sunSide4 = document.getElementById('side4');
+let sunSide5 = document.getElementById('side5');
+let sunSide6 = document.getElementById('side6');
+let sunSide7 = document.getElementById('side7');
+let sunSide8 = document.getElementById('side8');
+let sunSide9 = document.getElementById('side9');
+let sunSide10 = document.getElementById('side10');
+let sunSide11 = document.getElementById('side11');
+let sunSide12 = document.getElementById('side12');
+let sunSide13 = document.getElementById('side13');
+let sunSide14 = document.getElementById('side14');
+let sunSide15 = document.getElementById('side15');
+let sunSide16 = document.getElementById('side16');
+let sunSide17 = document.getElementById('side17');
+let sunSide18 = document.getElementById('side18');
+let sunSide19 = document.getElementById('side19');
+let sunSide20 = document.getElementById('side20');
+let sunSide21 = document.getElementById('side21');
+let sunSide22 = document.getElementById('side22');
+let sunSide23 = document.getElementById('side23');
+let sunSide24 = document.getElementById('side24');
 
 var requestURLtwo = 'https://entree-f18.herokuapp.com/v1/menu/12';
 var requesttwo = new XMLHttpRequest();
@@ -29,7 +55,142 @@ requesttwo.onload = function () {
     sunMenuItem8.innerHTML = requesttwo.response.menu_items[7].description;
     sunMenuItem9.innerHTML = requesttwo.response.menu_items[8].description;
     sunMenuItem10.innerHTML = requesttwo.response.menu_items[9].description;
+    sunMenuItem11.innerHTML = requesttwo.response.menu_items[10].description;
+    sunMenuItem12.innerHTML = requesttwo.response.menu_items[11].description;
     console.log(requesttwo.response.menu_items[0].description);
+
+    let y = "with";
+
+    let side1string = sunMenuItem1.innerHTML.slice(sunMenuItem1.innerHTML.indexOf(y) + y.length);
+    console.log(side1string);
+    let array = side1string.split(' and');
+    console.log(array);
+    let side1 = array[0];
+    sunSide1.innerHTML = side1;
+    let side2 = array[1];
+    console.log(side2);
+    sunSide2.innerHTML = side2;
+
+    let side2string = sunMenuItem2.innerHTML.slice(sunMenuItem2.innerHTML.indexOf(y) + y.length);
+    console.log(side2string);
+    let array2 = side2string.split(' and');
+    console.log(array2);
+    let side3 = array2[0];
+    console.log(side3);
+    sunSide3.innerHTML = side3;
+    let side4 = array2[1];
+    console.log(side4);
+    sunSide4.innerHTML = side4;
+
+    let side3string = sunMenuItem3.innerHTML.slice(sunMenuItem3.innerHTML.indexOf(y) + y.length);
+    console.log(side3string);
+    let array3 = side3string.split(' and');
+    console.log(array3);
+    let side5 = array3[0];
+    console.log(side5);
+    sunSide5.innerHTML = side5;
+    let side6 = array3[1];
+    console.log(side6);
+    sunSide6.innerHTML = side6;
+
+    let side4string = sunMenuItem4.innerHTML.slice(sunMenuItem4.innerHTML.indexOf(y) + y.length);
+    console.log(side4string);
+    let array4 = side4string.split(' and');
+    console.log(array4);
+    let side7 = array4[0];
+    console.log(side7);
+    sunSide7.innerHTML = side7;
+    let side8 = array4[1];
+    console.log(side8);
+    sunSide8.innerHTML = side8;
+
+    let side5string = sunMenuItem5.innerHTML.slice(sunMenuItem5.innerHTML.indexOf(y) + y.length);
+    console.log(side5string);
+    let array5 = side5string.split(' and');
+    console.log(array5);
+    let side9 = array5[0];
+    console.log(side9);
+    sunSide9.innerHTML = side9;
+    let side10 = array5[1];
+    console.log(side10);
+    sunSide10.innerHTML = side10;
+
+    let side6string = sunMenuItem6.innerHTML.slice(sunMenuItem6.innerHTML.indexOf(y) + y.length);
+    console.log(side6string);
+    let array6 = side6string.split(' and');
+    console.log(array6);
+    let side11 = array6[0];
+    console.log(side11);
+    sunSide11.innerHTML = side11;
+    let side12 = array6[1];
+    console.log(side12);
+    sunSide12.innerHTML = side12;
+
+    let side7string = sunMenuItem7.innerHTML.slice(sunMenuItem7.innerHTML.indexOf(y) + y.length);
+    console.log(side7string);
+    let array7 = side7string.split(' and');
+    console.log(array7);
+    let side13 = array7[0];
+    console.log(side13);
+    sunSide13.innerHTML = side13;
+    let side14 = array7[1];
+    console.log(side14);
+    sunSide14.innerHTML = side14;
+
+    let side8string = sunMenuItem8.innerHTML.slice(sunMenuItem8.innerHTML.indexOf(y) + y.length);
+    console.log(side8string);
+    let array8 = side8string.split(' and');
+    console.log(array8);
+    let side15 = array8[0];
+    sunSide15.innerHTML = side15;
+    console.log(side15);
+    let side16 = array8[1];
+    console.log(side16);
+    sunSide16.innerHTML = side16;
+
+    let side9string = sunMenuItem9.innerHTML.slice(sunMenuItem9.innerHTML.indexOf(y) + y.length);
+    console.log(side9string);
+    let array9 = side9string.split(' and');
+    console.log(array9);
+    let side17 = array9[0];
+    console.log(side17);
+    sunSide17.innerHTML = side17;
+    let side18 = array9[1];
+    console.log(side18);
+    sunSide18.innerHTML = side18;
+
+    let side10string = sunMenuItem10.innerHTML.slice(sunMenuItem10.innerHTML.indexOf(y) + y.length);
+    console.log(side10string);
+    let array10 = side10string.split(' and');
+    console.log(array10);
+    let side19 = array10[0];
+    console.log(side19);
+    sunSide19.innerHTML = side19;
+    let side20 = array10[1];
+    console.log(side20);
+    sunSide20.innerHTML = side20;
+
+    let side11string = sunMenuItem11.innerHTML.slice(sunMenuItem11.innerHTML.indexOf(y) + y.length);
+    console.log(side11string);
+    let array11 = side11string.split(' and');
+    console.log(array11);
+    let side21 = array11[0];
+    console.log(side21);
+    sunSide21.innerHTML = side21;
+    let side22 = array11[1];
+    console.log(side22);
+    sunSide22.innerHTML = side22;
+
+    let side12string = sunMenuItem12.innerHTML.slice(sunMenuItem12.innerHTML.indexOf(y) + y.length);
+    console.log(side12string);
+    let array12 = side12string.split(' and');
+    console.log(array);
+    let side23 = array12[0];
+    console.log(side23);
+    sunSide23.innerHTML = side23;
+    let side24 = array12[1];
+    console.log(side24);
+    sunSide24.innerHTML = side24;
   } else {
     alert("Connection to the menu isn't working, try again later.");
   }
--- src/js/wed.js
+++ src/js/wed.js
@@ -8,7 +8,26 @@ let wedMenuItem7 = document.getElementById('wedMenuItem7');
 let wedMenuItem8 = document.getElementById('wedMenuItem8');
 let wedMenuItem9 = document.getElementById('wedMenuItem9');
 let wedMenuItem10 = document.getElementById('wedMenuItem10');
-let side1 = document.getElementById('side1');
+let wedSide1 = document.getElementById('side1');
+let wedSide2 = document.getElementById('side2');
+let wedSide3 = document.getElementById('side3');
+let wedSide4 = document.getElementById('side4');
+let wedSide5 = document.getElementById('side5');
+let wedSide6 = document.getElementById('side6');
+let wedSide7 = document.getElementById('side7');
+let wedSide8 = document.getElementById('side8');
+let wedSide9 = document.getElementById('side9');
+let wedSide10 = document.getElementById('side10');
+let wedSide11 = document.getElementById('side11');
+let wedSide12 = document.getElementById('side12');
+let wedSide13 = document.getElementById('side13');
+let wedSide14 = document.getElementById('side14');
+let wedSide15 = document.getElementById('side15');
+let wedSide16 = document.getElementById('side16');
+let wedSide17 = document.getElementById('side17');
+let wedSide18 = document.getElementById('side18');
+let wedSide19 = document.getElementById('side19');
+let wedSide20 = document.getElementById('side20');
 
 var requestURL = 'https://entree-f18.herokuapp.com/v1/menu';
 var request = new XMLHttpRequest();
@@ -40,9 +59,10 @@ request.onload = function() {
       let array = side1string.split(' and');
       console.log(array);
       let side1 = array[0];
-      console.log(side1);
+      wedSide1.innerHTML = side1;
       let side2 = array[1];
       console.log(side2);
+      wedSide2.innerHTML = side2;
 
       let side2string = wedMenuItem2.innerHTML.slice(wedMenuItem2.innerHTML.indexOf(y) + y.length);
       console.log(side2string);
@@ -50,8 +70,10 @@ request.onload = function() {
       console.log(array2);
       let side3 = array2[0];
       console.log(side3);
+      wedSide3.innerHTML = side3;
       let side4 = array2[1];
       console.log(side4);
+      wedSide4.innerHTML = side4;
 
       let side3string = wedMenuItem3.innerHTML.slice(wedMenuItem3.innerHTML.indexOf(y) + y.length);
       console.log(side3string);
@@ -59,8 +81,10 @@ request.onload = function() {
       console.log(array3);
       let side5 = array3[0];
       console.log(side5);
+      wedSide5.innerHTML = side5;
       let side6 = array3[1];
       console.log(side6);
+      wedSide6.innerHTML = side6;
 
       let side4string = wedMenuItem4.innerHTML.slice(wedMenuItem4.innerHTML.indexOf(y) + y.length);
       console.log(side4string);
@@ -68,8 +92,10 @@ request.onload = function() {
       console.log(array4);
       let side7 = array4[0];
       console.log(side7);
+      wedSide7.innerHTML = side7;
       let side8 = array4[1];
       console.log(side8);
+      wedSide8.innerHTML = side8;
 
       let side5string = wedMenuItem5.innerHTML.slice(wedMenuItem5.innerHTML.indexOf(y) + y.length);
       console.log(side5string);
@@ -77,8 +103,10 @@ request.onload = function() {
       console.log(array5);
       let side9 = array5[0];
       console.log(side9);
+      wedSide9.innerHTML = side9;
       let side10 = array5[1];
       console.log(side10);
+      wedSide10.innerHTML = side10;
 
       let side6string = wedMenuItem6.innerHTML.slice(wedMenuItem6.innerHTML.indexOf(y) + y.length);
       console.log(side6string);
@@ -86,8 +114,10 @@ request.onload = function() {
       console.log(array6);
       let side11 = array6[0];
       console.log(side11);
+      wedSide11.innerHTML = side11;
       let side12 = array6[1];
       console.log(side12);
+      wedSide12.innerHTML = side12;
 
       let side7string = wedMenuItem7.innerHTML.slice(wedMenuItem7.innerHTML.indexOf(y) + y.length);
       console.log(side7string);
@@ -95,17 +125,21 @@ request.onload = function() {
       console.log(array7);
       let side13 = array7[0];
       console.log(side13);
+      wedSide13.innerHTML = side13;
       let side14 = array7[1];
       console.log(side14);
+      wedSide14.innerHTML = side14;
 
       let side8string = wedMenuItem8.innerHTML.slice(wedMenuItem8.innerHTML.indexOf(y) + y.length);
       console.log(side8string);
       let array8 = side8string.split(' and');
       console.log(array8);
       let side15 = array8[0];
+      wedSide15.innerHTML = side15;
       console.log(side15);
       let side16 = array8[1];
       console.log(side16);
+      wedSide16.innerHTML = side16;
 
       let side9string = wedMenuItem9.innerHTML.slice(wedMenuItem9.innerHTML.indexOf(y) + y.length);
       console.log(side9string);
@@ -113,8 +147,10 @@ request.onload = function() {
       console.log(array9);
       let side17 = array9[0];
       console.log(side17);
+      wedSide17.innerHTML = side17;
       let side18 = array9[1];
       console.log(side18);
+      wedSide18.innerHTML = side18;
 
       let side10string = wedMenuItem10.innerHTML.slice(wedMenuItem10.innerHTML.indexOf(y) + y.length);
       console.log(side10string);
@@ -122,8 +158,10 @@ request.onload = function() {
       console.log(array);
       let side19 = array10[0];
       console.log(side1);
+      wedSide19.innerHTML = side19;
       let side20 = array10[1];
       console.log(side20);
+      wedSide20.innerHTML = side20;
 
 
   }
